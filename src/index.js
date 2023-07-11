923c1024b3b7c38cdbd70a6df9a68958b0630e4a
mykola dychkevych <mykola.dychkevych@sigma.software>
2023-07-11 16:46:31 +0300

    added redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './components/app/App';
+
 import { DevSupport } from '@react-buddy/ide-toolbox';
 import { ComponentPreviews, useInitial } from './dev';
 
+import { store, persistor } from './store';
+
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
-    <React.StrictMode>
-      <Router>
-        <DevSupport ComponentPreviews={ ComponentPreviews }
-          useInitialHook={ useInitial }
-        >
-          <App/>
-        </DevSupport>
-      </Router>
-    </React.StrictMode>,
+    <Provider store={ store }>
+      <PersistGate loading={ null } persistor={ persistor }>
+        <Router>
+          <DevSupport ComponentPreviews={ ComponentPreviews }
+            useInitialHook={ useInitial }
+          >
+            <App/>
+          </DevSupport>
+        </Router>
+      </PersistGate>
+    </Provider>,
 );
